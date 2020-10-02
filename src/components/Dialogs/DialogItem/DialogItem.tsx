a0df58e66936d4a2b354b00c0d3d25cd66e60ce6
Nikolay Alexeev <kodi_kodi_kodi@mail.ru>
2020-10-02 22:33:08 +0500

    some refactoring to reducers

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -1,7 +1,7 @@
 import s from "../Dialogs.module.css";
 import {NavLink} from "react-router-dom";
 import React from "react";
-import { StateDialogsPageDialogsItemType } from "../../../redux/store";
+import { StateDialogsPageDialogsItemType } from "../../../redux/DialogsPageReducer";
 
 type DialogItemPropsType = StateDialogsPageDialogsItemType
 
--- src/redux/reduxStore.ts
+++ src/redux/reduxStore.ts
@@ -3,13 +3,15 @@ import {profilePageReducer} from "./ProfilePageReducer";
 import {dialogsPageReducer} from "./DialogsPageReducer";
 import {usersPageReducer} from "./UsersPageReducer";
 
-const reducers = combineReducers({
+const rootReducer = combineReducers({
     profilePage: profilePageReducer,
     dialogsPage: dialogsPageReducer,
     usersPage: usersPageReducer
 })
 
-export let store = createStore(reducers,
+export type RootStateType = ReturnType<typeof rootReducer>
+
+export let store = createStore(rootReducer,
     //@ts-ignore
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 //@ts-ignore
