303879f7bc296a4c48353e513d4fa304e75ee871
Nikolay Alexeev <kodi_kodi_kodi@mail.ru>
2020-10-25 18:52:00 +0500

    Updates

---
+++ precache-manifest.7ad68168f5b69abbef60a442d9cffb55.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4d147a2645d1b5816a3ab2bc7cca0c",
+    "url": "/react-social-network/index.html"
+  },
+  {
+    "revision": "3e6af9dadf79f3e276c9",
+    "url": "/react-social-network/static/css/3.1e9a5751.chunk.css"
+  },
+  {
+    "revision": "ebb1f0f14e4d61240315",
+    "url": "/react-social-network/static/css/4.2beaa6d1.chunk.css"
+  },
+  {
+    "revision": "db2e072ecef777096938",
+    "url": "/react-social-network/static/css/main.f5babbca.chunk.css"
+  },
+  {
+    "revision": "3bae3500d816f75cd235",
+    "url": "/react-social-network/static/js/2.b7e85909.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-social-network/static/js/2.b7e85909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6af9dadf79f3e276c9",
+    "url": "/react-social-network/static/js/3.f6f3c4fe.chunk.js"
+  },
+  {
+    "revision": "ebb1f0f14e4d61240315",
+    "url": "/react-social-network/static/js/4.a64338b1.chunk.js"
+  },
+  {
+    "revision": "db2e072ecef777096938",
+    "url": "/react-social-network/static/js/main.0e08604b.chunk.js"
+  },
+  {
+    "revision": "35d7660b675f7054ec9c",
+    "url": "/react-social-network/static/js/runtime-main.8b698060.js"
+  },
+  {
+    "revision": "337899dfc3c526d0c0b63d8d35a11dad",
+    "url": "/react-social-network/static/media/loading-loader-svgrepo-com.337899df.svg"
+  }
+]);
